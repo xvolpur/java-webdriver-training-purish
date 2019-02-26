df30effa835d732a25e577a937a1cf9f3989158b
xvolpur <volodymyr.purish@globallogic.com>
2019-02-26 17:43:55 +0200

    litecart appearence pages done

--- src/test/java/CampaignsYellowDuck.java
+++ src/test/java/CampaignsYellowDuck.java
@@ -33,21 +33,25 @@ public class CampaignsYellowDuck {
 
         System.out.println("TestsYellow Duck Started");
 
-        WebDriverManager.chromedriver().setup();
-        drvChrome = new ChromeDriver();
+       // WebDriverManager.chromedriver().setup();
+        //drvChrome = new ChromeDriver();
 
         //System.setProperty("webdriver.gecko.driver", driverPathFirefox);
         //drvFirefox = new FirefoxDriver();
 
-        //System.setProperty("webdriver.edge.driver",driverPathEdge);
-        //drvEdge = new EdgeDriver();
+        System.setProperty("webdriver.edge.driver",driverPathEdge);
+        drvEdge = new EdgeDriver();
 
-        wait =new WebDriverWait(drvChrome, 12);
+        //wait =new WebDriverWait(drvChrome, 12);
+       // wait =new WebDriverWait(drvFirefox, 12);
+        wait =new WebDriverWait(drvEdge, 12);
         //driver.manage().window().maximize();
 
-        drvChrome.navigate().to(baseURL);
-       // drvFirefox.navigate().to(baseURL);
-       // drvEdge.navigate().to(baseURL);
+        //drvChrome.navigate().to(baseURL);
+       // campaing();
+        //drvFirefox.navigate().to(baseURL);
+        //campaing_fireFox();
+       drvEdge.navigate().to(baseURL);
 
     }
 
@@ -55,8 +59,135 @@ public class CampaignsYellowDuck {
     public void campaings__cleanup() {
       drvChrome.quit();
      //drvFirefox.quit();
-     //drvEdge.quit();
+     drvEdge.quit();
         System.out.println("Tests Yellow Duck Finished");
+    }
+
+    @Test
+    public void campaing_Edge(){
+        System.out.println("begin Edge proc ");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#fancybox-title")));
+        System.out.println("Edge test is started ");
+        //                                       Main Page
+        //***********************************************************************************************************
+        WebElement mainCampaing = drvEdge.findElement(By.cssSelector("#box-campaigns"));
+
+        String regPriceMainCampaing = mainCampaing.findElement(By.cssSelector(".regular-price")).getText();
+
+        String discontPriceCampaing = mainCampaing.findElement(By.cssSelector(".campaign-price")).getText();
+
+        String mainPageName = mainCampaing.findElement(By.cssSelector(".name")).getText();
+
+        System.out.println("Edge test point 3");
+
+        // c.	Regular price is gray and strike ( ) on Main page
+        Assert.assertEquals( mainCampaing.findElement(By.cssSelector(".regular-price")).getCssValue("color"),"rgb(119, 119, 119)");
+        /*    Expected =  Actual   */
+
+        // Assert.assertEquals( mainCampaing.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration-line:"),"line-through");   same with Chrome
+/*    Expected :
+      Actual   :line-through   */
+
+        //   d.	Campaigns price is red and bold on Main page
+        Assert.assertEquals(mainCampaing.findElement(By.cssSelector(".campaign-price")).getCssValue("color"),"rgb(204, 0, 0)" );
+        /*    Expected =  Actual   */
+
+        Assert.assertEquals(mainCampaing.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight"), "700");
+        /*    Expected =  Actual   */
+        System.out.println("Edge test point 4 - Item will be started");
+        //                              ITEM
+        //***************************************************************************************************
+        mainCampaing.findElement(By.cssSelector(".name")).click();
+        System.out.println("Edge test point 5 - Item is started");
+        /////WebElement item =  drvEdge.findElement(By.id("add_cart_product"));
+        System.out.println("Edge test point 6 ");
+        // a.	Product Name is equal on Main page and on Item Page
+        Assert.assertEquals(drvEdge.findElement(By.cssSelector("h1.title")).getText(),mainPageName );
+        System.out.println("Edge test point 7 ");
+        // b.	Prices (discount and regular) are equal on both pages
+        Assert.assertEquals(drvEdge.findElement(By.cssSelector(".regular-price")).getText(),regPriceMainCampaing );
+        System.out.println("Edge test point 8 ");
+        Assert.assertEquals(drvEdge.findElement(By.cssSelector(".campaign-price")).getText(),discontPriceCampaing );
+        System.out.println("Edge test point 9 ");
+//        c.	Regular price is gray and strike ( ) on ITEM page
+        Assert.assertEquals(drvEdge.findElement(By.cssSelector(".regular-price")).getCssValue("color"),"rgb(102, 102, 102)");
+        /*    Expected =  Actual   */
+        System.out.println("Edge test point 10 ");
+        // Assert.assertEquals(item.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration-line:"),"line-through");
+/*    Expected :
+      Actual   :line-through    */
+
+        //    d.	Campaigns price is red and bold on both pages
+        Assert.assertEquals(drvEdge.findElement(By.cssSelector(".campaign-price")).getCssValue("color"),"rgb(204, 0, 0)" );
+        /*    Expected =  Actual   */
+        System.out.println("Edge test point 11 ");
+
+        Assert.assertEquals(drvEdge.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight"),"700" );
+        /*    Expected =  Actual   */
+        System.out.println("Edge test is done ");
+    }
+
+
+    @Test
+    public void campaing_fireFox(){
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#fancybox-title")));
+        System.out.println("Firefox test is started ");
+        //                                       Main Page
+        //***********************************************************************************************************
+        WebElement mainCampaing = drvFirefox.findElement(By.cssSelector("#box-campaigns"));
+
+        String regPriceMainCampaing = mainCampaing.findElement(By.cssSelector(".regular-price")).getText();
+
+        String discontPriceCampaing = mainCampaing.findElement(By.cssSelector(".campaign-price")).getText();
+
+        String mainPageName = mainCampaing.findElement(By.cssSelector(".name")).getText();
+
+        // c.	Regular price is gray and strike ( ) on Main page
+        Assert.assertEquals( mainCampaing.findElement(By.cssSelector(".regular-price")).getCssValue("color"),"rgb(119, 119, 119)");
+        /*    Expected =  Actual   */
+
+       // Assert.assertEquals( mainCampaing.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration-line:"),"line-through");   same with Chrome
+/*    Expected :
+      Actual   :line-through   */
+
+        //   d.	Campaigns price is red and bold on Main page
+        Assert.assertEquals(mainCampaing.findElement(By.cssSelector(".campaign-price")).getCssValue("color"),"rgb(204, 0, 0)" );
+        /*    Expected =  Actual   */
+
+        Assert.assertEquals(mainCampaing.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight"), "900");
+        /*    Expected =  Actual   */
+
+        //                              ITEM
+        //***************************************************************************************************
+        mainCampaing.findElement(By.cssSelector(".name")).click();
+
+        WebElement item =  drvFirefox.findElement(By.cssSelector("div.information"));
+
+        // a.	Product Name is equal on Main page and on Item Page
+        Assert.assertEquals(drvFirefox.findElement(By.cssSelector("h1.title")).getText(),mainPageName );
+
+        // b.	Prices (discount and regular) are equal on both pages
+        Assert.assertEquals(item.findElement(By.cssSelector(".regular-price")).getText(),regPriceMainCampaing );
+
+        Assert.assertEquals(item.findElement(By.cssSelector(".campaign-price")).getText(),discontPriceCampaing );
+
+//        c.	Regular price is gray and strike ( ) on ITEM page
+        Assert.assertEquals(item.findElement(By.cssSelector(".regular-price")).getCssValue("color"),"rgb(102, 102, 102)");
+        /*    Expected =  Actual   */
+
+        Assert.assertEquals(item.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration-line:"),"line-through");
+/*    Expected :
+      Actual   :line-through    */
+
+        //    d.	Campaigns price is red and bold on both pages
+        Assert.assertEquals(item.findElement(By.cssSelector(".campaign-price")).getCssValue("color"),"rgb(204, 0, 0)" );
+         /*    Expected =  Actual   */
+
+
+        Assert.assertEquals(item.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight"),"700" );
+        /*    Expected =  Actual   */
+        System.out.println("Firefox test is done ");
     }
 
     @Test
@@ -123,4 +254,6 @@ public class CampaignsYellowDuck {
 /*    Expected =  Actual   */
 
     }
+
+
 }
---
+++ src/test/java/Task5.java
@@ -0,0 +1,115 @@
+import org.junit.After;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static javafx.scene.paint.Color.rgb;
+
+public class Task5 {
+
+
+    private WebDriver drvChrome;
+    private WebDriverWait wait;
+    private String baseURL = "http://localhost/litecart/admin";
+
+    private String username = "admin";
+    private String password = "admin";
+
+    @Before
+    public void task5_setup() {
+
+        System.out.println("Task5 is Started");
+
+        WebDriverManager.chromedriver().setup();
+        drvChrome = new ChromeDriver();
+
+        wait =new WebDriverWait(drvChrome, 18);
+       //driver.manage().window().maximize();
+         performLogin();
+    }
+
+    @After
+    public void taskt5_cleanup() {
+        // drvChrome.quit();
+                System.out.println("Tests5 is Finished");
+    }
+
+    private void performLogin(){
+        By sidebar = By.id("sidebar");
+
+        drvChrome.navigate().to(baseURL);
+
+      if (isElementPresent(sidebar)) return;
+
+        drvChrome.findElement(By.name("username")).sendKeys(username);
+        drvChrome.findElement(By.name("password")).sendKeys(password);
+        drvChrome.findElement(By.name("login")).click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(sidebar));
+    }
+
+    private boolean isElementPresent(By element) {
+        return drvChrome.findElements(element).size() > 0;
+    }
+
+
+    @Test
+    public void task5_file(){
+
+        By menuBlock = By.cssSelector("#sidebar ul#box-apps-menu");
+        List<WebElement> menuItems = drvChrome.findElement(menuBlock).findElements(By.xpath("./li"));
+
+        int menuSize = drvChrome.findElement(menuBlock).findElements(By.xpath("./li")).size();
+
+        for (int menuItem = 1; menuItem <= menuSize; menuItem++) {
+
+            WebElement currentMenuItemName = drvChrome.findElement(menuBlock).findElement(By.xpath("./li[" + menuItem + "]"));
+
+            if ("Catalog".equals(currentMenuItemName.getText().trim())){  currentMenuItemName.click(); break; }
+        }
+
+          drvChrome.findElement(By.partialLinkText("Add New Product")).click();
+
+        //   Add New Product General
+        //*********************************************************
+
+        // returns the current time in milliseconds + substring
+        System.out.print("Current Time in milliseconds = ");
+        System.out.println(System.currentTimeMillis());
+
+        long currentMillSec = System.currentTimeMillis();
+        String itemName = "Item"+String.valueOf(currentMillSec).substring(7,13);
+        //populate field Name
+        WebElement inputName = drvChrome.findElement(By.cssSelector("[name*=name]"));
+        inputName.sendKeys(itemName);
+
+
+
+     // populate upload file image    new_images[]
+        WebElement uploadImage = drvChrome.findElement(By.cssSelector("[name*=new_images]"));
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("book.png").getFile());
+        uploadImage.sendKeys(file.getAbsolutePath());
+
+        System.out.println(file.getAbsolutePath());
+        // printed  D:\src\out\test\resources\book.png
+
+
+    }
+}
